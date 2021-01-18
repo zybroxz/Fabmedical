b0b62e01cd3a0a723d8f363ea55cbf06f3b48e93
Darren Willoughby <willougd@gmail.com>
2021-01-18 14:00:03 +0000

    Added Environment Variables

--- content-web/app.js
+++ content-web/app.js
@@ -6,7 +6,7 @@ const request = require('request');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
-const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
+const contentApiUrl = process.env.CONTENT_API_URL || "http://13.64.196.242:3001";
 
 
 function getSessions(cb) {
